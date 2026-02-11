d9866f56c97b9dee986d1915b86644256382c29a
Duda Sena <duda.sruiz@gmail.com>
2026-02-11 00:23:56 -0300

    chore: initial spring boot setup with dependencies

---
+++ src/main/java/com/pethealth/pet_api/PetHealthApplication.java
@@ -0,0 +1,13 @@
+package com.pethealth.pet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHealthApplication.class, args);
+	}
+
+}
